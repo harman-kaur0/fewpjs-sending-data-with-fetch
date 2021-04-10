fe9b48ef8ad82ede0507f15c90e65f4d9680f364
Harmandeep Kaur <harmandeepk628@gmail.com>
2021-04-10 00:28:03 -0700

    commit tree b921b59903f9d311a440442bdcfb4a94ea5bbc7b

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Add your code here
+let data = {
+  name: "Steve",
+  email: "steve@steve.com",
+};
+
+let config = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify(data),
+};
+
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", config)
+    .then((response) => 
+      response.json()
+    )
+    .then((object) => {
+      let p = document.createElement("p");
+      p.innerHTML = object["id"];
+      document.body.appendChild(p);
+    })
+    .catch((error) => {
+      let p = document.createElement("p");
+      p.innerHTML = "Unauthorized Access";
+      document.body.appendChild(p);
+    });
+}
+
+// let data = {
+//   name: "Steve",
+//   email: "steve@steve.com",
+// };
+
+// let config = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Accept: "application/json",
+//   },
+//   body: JSON.stringify(data),
+// };
+
+// function submitData(name, email) {
+//   return fetch("http://localhost:3000/users", config)
+//     .then((resp) => resp.json())
+//     .then((object) => {
+//       let p = document.createElement("p");
+//       p.innerHTML = object["id"];
+//       document.body.appendChild(p);
+//     })
+//     .catch((error) => {
+//       let p = document.createElement("p");
+//       p.innerHTML = "Unauthorized Access";
+//       document.body.appendChild(p);
+//     });
+// }
